2b76471a96bb93ed5d6052deb97ae6f8b90ff5d7
asdammantap <asdamgrimson@gmail.com>
2019-11-22 07:33:05 +0700

    modify and add diagram dashboard

--- application/modules/app/views/dashboard/dashboard.php
+++ application/modules/app/views/dashboard/dashboard.php
@@ -44,7 +44,11 @@
 			<div class="tile-stats">
 				<div class="icon"><i class="fa fa-users"></i>
 				</div>
-				<div class="count">20</div>
+				<div class="count">
+					<?php 
+					$query = $this->db->query("SELECT * FROM view_detailkeluarga");
+					echo $query->num_rows();
+					?></div>
 
 				<h3>Jumlah Warga RT.007</h3>
 				<!-- <p>Tentang Simulasi Angsuran</p> -->
@@ -79,12 +83,13 @@
 			<div class="col-md-12 col-sm-6 col-xs-12">
                 <div class="x_panel">
                   <div class="x_title">
-                    <h2>Charts <small>By Age <?php echo $hasilnik->num_rows();?></small></h2>
+                    <h2>Charts <small>By Age</small></h2>
                     
                     <div class="clearfix"></div>
                   </div>
                   <div class="x_content">
-                    <div id="graph_bar" style="width:100%; height:280px;"></div>
+                    <!-- <div id="graph_bar" style="width:100%; height:280px;"></div> -->
+					<canvas id="diagramusia"></canvas>
                   </div>
                 </div>
               </div>
@@ -103,6 +108,7 @@
 		
 		<script>
 		var ctx = document.getElementById("diagramgender").getContext('2d');
+		var ctxage = document.getElementById("diagramusia").getContext('2d');
 		var myChart = new Chart(ctx, {
 			type: 'bar',
 			data: {
@@ -111,11 +117,11 @@
 					label: '',
 					data: [
 					<?php 
-					$querylaki = $this->db->query("SELECT * FROM tbl_kkdetail WHERE wrg_jeniskel like '%Laki%'");
+					$querylaki = $this->db->query("SELECT * FROM view_detailkeluarga WHERE wrg_jeniskel like '%Laki%'");
 					echo $querylaki->num_rows();
 					?>, 
 					<?php 
-					$queryperempuan = $this->db->query("SELECT * FROM tbl_kkdetail WHERE wrg_jeniskel like '%Perempuan%'");
+					$queryperempuan = $this->db->query("SELECT * FROM view_detailkeluarga WHERE wrg_jeniskel like '%Perempuan%'");
 					echo $queryperempuan->num_rows();
 					?>, 
 					
@@ -129,6 +135,63 @@
 					borderColor: [
 					'rgba(54, 162, 235, 1)',
 					'rgba(255,99,132,1)'
+					
+					],
+					borderWidth: 1
+				}]
+			},
+			options: {
+				scales: {
+					yAxes: [{
+						ticks: {
+							beginAtZero:true
+						}
+					}]
+				}
+			}
+		});
+		var myChartage = new Chart(ctxage, {
+			type: 'bar',
+			data: {
+				labels: ["0-10", "11-20","21-30", "31-50","Lebih dari 50"],
+				datasets: [{
+					label: '',
+					data: [
+					<?php 
+					$query010 = $this->db->query("SELECT * FROM view_detailkeluarga WHERE wrg_usia BETWEEN 0 AND 10");
+					echo $query010->num_rows();
+					?>, 
+					<?php 
+					$query1120 = $this->db->query("SELECT * FROM view_detailkeluarga WHERE wrg_usia BETWEEN 11 AND 20");
+					echo $query1120->num_rows();
+					?>, 
+					<?php 
+					$query2130 = $this->db->query("SELECT * FROM view_detailkeluarga WHERE wrg_usia BETWEEN 21 AND 30");
+					echo $query2130->num_rows();
+					?>, 
+					<?php 
+					$query3150 = $this->db->query("SELECT * FROM view_detailkeluarga WHERE wrg_usia BETWEEN 31 AND 50");
+					echo $query3150->num_rows();
+					?>, 
+					<?php 
+					$queryLB50 = $this->db->query("SELECT * FROM view_detailkeluarga WHERE wrg_usia > 50");
+					echo $queryLB50->num_rows();
+					?>
+					
+					],
+					backgroundColor: [
+					'rgba(54, 162, 235, 0.2)',
+					'rgba(255, 99, 132, 0.2)',
+					'rgba(54, 162, 235, 0.2)',
+					'rgba(255, 99, 132, 0.2)',
+					'rgba(54, 162, 235, 0.2)'
+					],
+					borderColor: [
+					'rgba(54, 162, 235, 1)',
+					'rgba(255,99,132,1)',
+					'rgba(54, 162, 235, 1)',
+					'rgba(255,99,132,1)',
+					'rgba(54, 162, 235, 1)'
 					
 					],
 					borderWidth: 1
